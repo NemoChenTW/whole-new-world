f0286e328a1a3b855de393be1abbb41a4669691e
Nemo Chen <Nemo.Chen@tomtom.com>
2019-06-13 18:06:39 +0800

    Import map sdk & display map

--- app/src/main/java/com/taipei/ttbootcamp/MainActivity.java
+++ app/src/main/java/com/taipei/ttbootcamp/MainActivity.java
@@ -1,13 +1,40 @@
 package com.taipei.ttbootcamp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tomtom.online.sdk.map.MapFragment;
+import com.tomtom.online.sdk.map.OnMapReadyCallback;
+import com.tomtom.online.sdk.map.TomtomMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TomtomMap tomtomMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getAsyncMap(onMapReadyCallback);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        tomtomMap.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private final OnMapReadyCallback onMapReadyCallback =
+            new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(TomtomMap map) {
+                    //Map is ready here
+                    tomtomMap = map;
+                    tomtomMap.setMyLocationEnabled(true);
+//                    tomtomMap.collectLogsToFile(SampleApp.LOGCAT_PATH);
+                }
+            };
 }
