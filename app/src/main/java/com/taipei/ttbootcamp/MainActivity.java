94b81fb1203dde9a3f298c3063366a10f404685d
NemoChen <nemochentw@gmail.com>
2019-06-17 17:19:14 +0800

    Refactor

--- app/src/main/java/com/taipei/ttbootcamp/MainActivity.java
+++ app/src/main/java/com/taipei/ttbootcamp/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private TomtomMap tomtomMap;
     private SearchApi searchApi;
 
+    // UI items
     private ImageButton btnSearch;
     private EditText editTextPois;
 
@@ -38,10 +39,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initUIViews();
-
         MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getAsyncMap(onMapReadyCallback);
+
+        initUIViews();
         initTomTomServices();
 
         View.OnClickListener searchButtonListener = getSearchButtonListener();
