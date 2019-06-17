11bd2005786f22412ff4c912f22c8ba0e5f9a4f9
NemoChen <nemochentw@gmail.com>
2019-06-17 16:42:53 +0800

    Add search feature

--- app/src/main/java/com/taipei/ttbootcamp/MainActivity.java
+++ app/src/main/java/com/taipei/ttbootcamp/MainActivity.java
@@ -5,24 +5,89 @@ import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.tomtom.online.sdk.map.MapFragment;
 import com.tomtom.online.sdk.map.OnMapReadyCallback;
 import com.tomtom.online.sdk.map.TomtomMap;
 import com.tomtom.online.sdk.map.TomtomMapCallback;
+import com.tomtom.online.sdk.search.OnlineSearchApi;
+import com.tomtom.online.sdk.search.SearchApi;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchQueryBuilder;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResponse;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResult;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     static final String TAG = "MainActivity";
 
+    // TomTom service
     private TomtomMap tomtomMap;
+    private SearchApi searchApi;
+
+    private ImageButton btnSearch;
+    private EditText editTextPois;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initUIViews();
+
         MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         mapFragment.getAsyncMap(onMapReadyCallback);
+        initTomTomServices();
+
+        View.OnClickListener searchButtonListener = getSearchButtonListener();
+        btnSearch.setOnClickListener(searchButtonListener);
+    }
+    private void initTomTomServices() {
+        searchApi = OnlineSearchApi.create(this);
+    }
+
+    private View.OnClickListener getSearchButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleSearchClick(v);
+            }
+
+            private void handleSearchClick(View v) {
+                String textToSearch = editTextPois.getText().toString();
+                if (!textToSearch.isEmpty()) {
+                    doFuzzySearch(textToSearch);
+                }
+            }
+
+            private void doFuzzySearch(String textToSearch) {
+                Log.d(TAG, "doFuzzySearch= " + textToSearch);
+                final Integer QUERY_LIMIT = 10;
+                searchApi.search(new FuzzySearchQueryBuilder(textToSearch)
+                            .withLimit(QUERY_LIMIT).build())
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new DisposableSingleObserver<FuzzySearchResponse>() {
+
+                            @Override
+                            public void onSuccess(FuzzySearchResponse fuzzySearchResponse) {
+                                for (FuzzySearchResult fuzzySearchResult : fuzzySearchResponse.getResults()) {
+                                    Log.d(TAG, "fuzzySearchResult: " + fuzzySearchResult.toString());
+                                }
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.d(TAG, "Search error: " + e.getMessage());
+                            }
+                        });
+            }
+        };
     }
 
     @Override
@@ -68,5 +133,10 @@ public class MainActivity extends AppCompatActivity {
         String message = String.format(java.util.Locale.getDefault(),
                 "%s : %f : %f", title, lat, lon);
         Log.d(TAG, "displayMessage: " + message);
+    }
+
+    private void initUIViews() {
+        btnSearch = findViewById(R.id.btn_main_poisearch);
+        editTextPois = findViewById(R.id.edittext_main_poisearch);
     }
 }
