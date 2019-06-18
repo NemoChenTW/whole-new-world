c716f99e1fa5a84c5064d6fac596f3b8799b46af
Nick Chen <nick2100@gmail.com>
2019-06-18 14:48:53 +0800

    Add POI with travel time interface

---
+++ app/src/main/java/com/taipei/ttbootcamp/interfaces/POIWithTravelTime.java
@@ -0,0 +1,8 @@
+package com.taipei.ttbootcamp.interfaces;
+
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResult;
+
+public class POIWithTravelTime {
+    private FuzzySearchResult reqult;
+    private Integer travelTime;
+}
