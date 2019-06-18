8bd26a0c7fa29012b7047e5b3be799ccb08ef50b
Chih-Hung Liu <e1788hl@gmail.com>
2019-06-18 11:28:29 +0800

    Search POI with Type

---
+++ app/src/main/java/com/taipei/ttbootcamp/poigenerator/POIGenerator.java
@@ -0,0 +1,82 @@
+package com.taipei.ttbootcamp.poigenerator;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.tomtom.online.sdk.common.location.LatLng;
+import com.tomtom.online.sdk.common.location.LatLngAcc;
+import com.tomtom.online.sdk.search.OnlineSearchApi;
+import com.tomtom.online.sdk.search.SearchApi;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchQueryBuilder;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResponse;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResult;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class POIGenerator {
+    private final static String TAG = "POIGEN";
+
+    public enum POITYPE {
+        MUSEUM,
+        ART,
+        FOOD
+    }
+
+    private static final int STANDARD_RADIUS = 30 * 1000; //30 km
+
+    private static String convertPOITypeToText(POITYPE poitype) {
+        String ret = "";
+        switch (poitype)
+        {
+            case MUSEUM:
+                ret = "museum";
+                break;
+            case ART:
+                ret = "art";
+                break;
+            case FOOD:
+                ret = "food";
+                break;
+            default:
+                break;
+        }
+        return ret;
+    }
+
+    public static void getPOIsWithType(Context context, LatLng currentPosition, POITYPE poitype) {
+        if (context == null) {
+            Log.e(TAG, "Context is null.");
+            return;
+        }
+        SearchApi searchApi = OnlineSearchApi.create(context);
+
+        String textToSearch = convertPOITypeToText(poitype);
+
+        final Integer QUERY_LIMIT = 10;
+        searchApi.search(new FuzzySearchQueryBuilder(textToSearch)
+                .withPreciseness(new LatLngAcc(currentPosition, STANDARD_RADIUS))
+                .withTypeAhead(true)
+                .withCategory(true)
+                .withLimit(QUERY_LIMIT).build())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableSingleObserver<FuzzySearchResponse>() {
+
+                    @Override
+                    public void onSuccess(FuzzySearchResponse fuzzySearchResponse) {
+                        for (FuzzySearchResult fuzzySearchResult : fuzzySearchResponse.getResults()) {
+                            Log.d(TAG, "fuzzySearchResult: " + fuzzySearchResult.toString());
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "Search error: " + e.getMessage());
+                    }
+                });
+    }
+
+
+}
