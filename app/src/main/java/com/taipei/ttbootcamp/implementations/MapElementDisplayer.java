bacba97a184265176b1b5653ce008c0fc5a8d5a3
Nemo Chen <Nemo.Chen@tomtom.com>
2019-08-01 11:36:29 +0800

    Migrate to AndroidX

--- app/src/main/java/com/taipei/ttbootcamp/implementations/MapElementDisplayer.java
+++ app/src/main/java/com/taipei/ttbootcamp/implementations/MapElementDisplayer.java
@@ -1,8 +1,8 @@
 package com.taipei.ttbootcamp.implementations;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import android.util.Log;
 
 import com.google.common.base.Optional;
