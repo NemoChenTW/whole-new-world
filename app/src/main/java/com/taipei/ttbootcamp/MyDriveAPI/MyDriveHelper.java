f1d8bf1f0d93c5b656907eac08c8894a041cf1d4
Chih-Hung Liu <e1788hl@gmail.com>
2019-09-04 17:11:50 +0800

    Start implement MyDrive API

--- app/src/main/java/com/taipei/ttbootcamp/MyDriveAPI/MyDriveHelper.java
+++ app/src/main/java/com/taipei/ttbootcamp/MyDriveAPI/MyDriveHelper.java
@@ -1,4 +1,9 @@
 package com.taipei.ttbootcamp.MyDriveAPI;
 
+import com.tomtom.online.sdk.common.location.LatLng;
+
 public class MyDriveHelper {
+    public static void getNearestPublicItineraries(LatLng aLatLng) {
+
+    }
 }
